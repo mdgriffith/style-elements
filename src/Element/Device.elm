609fdd2e98351dd9de6948a7550fbfb4709611be
Matthew Griffith <mdg.griffith@gmail.com>
2017-04-22 15:59:17 -0400

    Device matches

--- src/Element/Device.elm
+++ src/Element/Device.elm
@@ -20,9 +20,9 @@ match : Window.Size -> Device
 match { width, height } =
     { width = width
     , height = height
-    , phone = True
-    , tablet = True
-    , desktop = True
-    , bigDesktop = True
+    , phone = width <= 600
+    , tablet = width > 600 && width <= 1200
+    , desktop = width > 1200 && width <= 1800
+    , bigDesktop = width > 1800
     , portrait = width > height
     }
