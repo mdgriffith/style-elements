dba5f58d2120c3be1dcef025480369429bee30f5
Matthew Griffith <mdg.griffith@gmail.com>
2017-05-22 16:39:36 -0400

    Move scales to their own module.

--- src/Style/Font.elm
+++ src/Style/Font.elm
@@ -1,8 +1,6 @@
 module Style.Font
     exposing
-        ( scale
-        , scaleInt
-        , typeface
+        ( typeface
         , size
         , height
         , letterSpacing
@@ -19,7 +17,7 @@ module Style.Font
         , noWrap
         , underline
         , strike
-        , italicize
+        , italics
         , bold
         , weight
         , light
@@ -30,79 +28,15 @@ module Style.Font
 
 {-| @docs typeface, size, height, letterSpacing, wordSpacing, left, right, center, justify, justifyAll
 
-@docs scale, scaleInt
-
 @docs wrap, pre, preWrap, preLine, noWrap
 
-@docs uppercase, capitalize, lowercase, underline, strike, italicize, bold, weight, light
+@docs uppercase, capitalize, lowercase, underline, strike, italics, bold, weight, light
 
 -}
 
 import Style.Internal.Model as Internal
 import Style.Internal.Render.Value as Value
 import Style exposing (Property)
-
-
-{-| When dealing with font sizes, it's nice to use a modular scale, which means all font sizes are related via a ratio.
-
-Here's how it's done, first create a scale:
-
-    scaled =
-        scale 16 1.618
-
-You can read this as "Starting at a base of 16, create a modular scale using the ratio 1.618."
-
-Then, when setting font sizes you can use:
-
-This will set the font size to 16px:
-
-    Font.size (scaled 1)
-
-Or we can scale up one level (which multiplies the result by 1.618):
-
-    Font.size (scaled 2)
-
-This results in a font size of 25.8px.
-
-We can also provide negative numbers to scale below 16px.
-
--}
-scale : Float -> Float -> Int -> Float
-scale normal ratio fontScale =
-    resize normal ratio fontScale
-
-
-{-| Same as `scale` but rounds the resunt to an `Int`
--}
-scaleInt : Float -> Float -> Int -> Int
-scaleInt normal ratio fontScale =
-    round <| resize normal ratio fontScale
-
-
-resize : Float -> Float -> Int -> Float
-resize normal ratio fontScale =
-    if fontScale == 0 || fontScale == 1 then
-        normal
-    else if fontScale < 0 then
-        shrink ratio (fontScale * -1) normal
-    else
-        grow ratio (fontScale - 1) normal
-
-
-grow : Float -> Int -> Float -> Float
-grow ratio i size =
-    if i <= 0 then
-        size
-    else
-        grow ratio (i - 1) (size * ratio)
-
-
-shrink : Float -> Int -> Float -> Float
-shrink ratio i size =
-    if i <= 0 then
-        size
-    else
-        shrink ratio (i - 1) (size / ratio)
 
 
 {-| -}
@@ -211,8 +145,8 @@ strike =
 
 
 {-| -}
-italicize : Property class variation animation
-italicize =
+italics : Property class variation animation
+italics =
     Internal.Font "font-style" "italics"
 
 
---
+++ src/Style/Scale.elm
@@ -0,0 +1,66 @@
+module Style.Scale exposing (modular, integer)
+
+{-| @docs modular, integer
+-}
+
+
+{-| When dealing with font sizes, it's nice to use a modular scale, which means all font sizes are related via a ratio.
+
+Here's how it's done, first create a scale:
+
+    scaled =
+        Scale.modular 16 1.618
+
+You can read this as "Starting at a base of 16, create a modular scale using the ratio 1.618."
+
+Then, when setting font sizes you can use:
+
+This will set the font size to 16px:
+
+    Font.size (scaled 1)
+
+Or we can scale up one level (which multiplies the result by 1.618):
+
+    Font.size (scaled 2)
+
+This results in a font size of 25.8px.
+
+We can also provide negative numbers to scale below 16px.
+
+-}
+modular : Float -> Float -> Int -> Float
+modular normal ratio fontScale =
+    resize normal ratio fontScale
+
+
+{-| Same as `scale` but rounds to an `Int`. This should be used for font sizes.
+-}
+integer : Float -> Float -> Int -> Int
+integer normal ratio fontScale =
+    round <| resize normal ratio fontScale
+
+
+resize : Float -> Float -> Int -> Float
+resize normal ratio fontScale =
+    if fontScale == 0 || fontScale == 1 then
+        normal
+    else if fontScale < 0 then
+        shrink ratio (fontScale * -1) normal
+    else
+        grow ratio (fontScale - 1) normal
+
+
+grow : Float -> Int -> Float -> Float
+grow ratio i size =
+    if i <= 0 then
+        size
+    else
+        grow ratio (i - 1) (size * ratio)
+
+
+shrink : Float -> Int -> Float -> Float
+shrink ratio i size =
+    if i <= 0 then
+        size
+    else
+        shrink ratio (i - 1) (size / ratio)
