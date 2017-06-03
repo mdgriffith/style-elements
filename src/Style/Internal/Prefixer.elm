7db12ce5919019255cc1b7969e9799b1bef3c332
Matthew Griffith <mdg.griffith@gmail.com>
2017-06-03 11:46:48 +0200

    Basic Prefixer (without data yet)

---
+++ src/Style/Internal/Prefixer.elm
@@ -0,0 +1,45 @@
+module Style.Internal.Prefixer exposing (prefix)
+
+{-| Style Prefixes
+-}
+
+
+{-| -}
+type Prefix
+    = Value String (List String)
+    | Property String (List String)
+
+
+{-| -}
+applyPrefix : Prefix -> ( String, String ) -> List ( String, String )
+applyPrefix prefix ( name, value ) =
+    case prefix of
+        Value valueMatch newValues ->
+            let
+                pair name value =
+                    ( name, value )
+            in
+                if value == valueMatch then
+                    ( name, value ) :: (List.map (pair name) newValues)
+                else
+                    [ ( name, value ) ]
+
+        Property nameMatch newNames ->
+            let
+                pairValue value name =
+                    ( name, value )
+            in
+                if name == nameMatch then
+                    ( name, value ) :: (List.map (pairValue value) newNames)
+                else
+                    [ ( name, value ) ]
+
+
+{-| -}
+prefix : List Prefix -> List ( String, String ) -> List ( String, String )
+prefix prefixes attrs =
+    let
+        applyAll attr prefixedAttrs =
+            List.foldr (\prefix prefixed -> applyPrefix prefix attr ++ prefixed) prefixedAttrs prefixes
+    in
+        List.foldr applyAll [] attrs
