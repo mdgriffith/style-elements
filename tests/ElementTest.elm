0b7670e76c854a90f6958b80ef1c64fa60b9857d
Matthew Griffith <mdg.griffith@gmail.com>
2017-06-15 11:05:29 -0400

    Link creation tests

--- tests/ElementTest.elm
+++ tests/ElementTest.elm
@@ -25,53 +25,69 @@ stylesheet =
         [ style None [] ]
 
 
-suite : Test
-suite =
-    describe "render"
-        [ describe "flex-shrink"
-            [ test "prevents fixed height child from shrinking inside a column" <|
-                \() ->
-                    Element.root stylesheet
-                        (column None
-                            []
-                            [ el None [] empty
-                            , el None [ height (px 200) ] empty
-                            ]
-                        )
-                        |> Query.fromHtml
-                        |> Query.has [ Selector.style [ ( "flex-shrink", "0" ) ] ]
-            , test "allows child with unspecified height to shrink inside a column" <|
-                \() ->
-                    Element.root stylesheet
-                        (row None
-                            []
-                            [ el None [] empty
-                            , el None [] empty
-                            ]
-                        )
-                        |> Query.fromHtml
-                        |> Query.hasNot [ Selector.style [ ( "flex-shrink", "0" ) ] ]
-            , test "prevents fixed width child from shrinking inside a row" <|
-                \() ->
-                    Element.root stylesheet
-                        (row None
-                            []
-                            [ el None [] empty
-                            , el None [ width (px 200) ] empty
-                            ]
-                        )
-                        |> Query.fromHtml
-                        |> Query.has [ Selector.style [ ( "flex-shrink", "0" ) ] ]
-            , test "allows child with unspecified width to shrink inside a row" <|
-                \() ->
-                    Element.root stylesheet
-                        (row None
-                            []
-                            [ el None [] empty
-                            , el None [] empty
-                            ]
-                        )
-                        |> Query.fromHtml
-                        |> Query.hasNot [ Selector.style [ ( "flex-shrink", "0" ) ] ]
-            ]
+linkTest : Test
+linkTest =
+    describe "link creation"
+        [ test "Create a normal link" <|
+            \() ->
+                Element.layout stylesheet
+                    (link "test" <| el None [ height (px 200) ] empty)
+                    |> Query.fromHtml
+                    |> Query.has [ Selector.tag "a", Selector.attribute "href" "test" ]
+        , test "Create a link from a layout" <|
+            \() ->
+                Element.layout stylesheet
+                    (link "test" <| row None [ height (px 200) ] [ el None [ height (px 200) ] empty ])
+                    |> Query.fromHtml
+                    |> Query.has [ Selector.tag "a", Selector.attribute "href" "test" ]
+        ]
+
+
+shrink : Test
+shrink =
+    describe "flex-shrink"
+        [ test "prevents fixed height child from shrinking inside a column" <|
+            \() ->
+                Element.layout stylesheet
+                    (column None
+                        []
+                        [ el None [] empty
+                        , el None [ height (px 200) ] empty
+                        ]
+                    )
+                    |> Query.fromHtml
+                    |> Query.has [ Selector.style [ ( "flex-shrink", "0" ) ] ]
+        , test "allows child with unspecified height to shrink inside a column" <|
+            \() ->
+                Element.layout stylesheet
+                    (row None
+                        []
+                        [ el None [] empty
+                        , el None [] empty
+                        ]
+                    )
+                    |> Query.fromHtml
+                    |> Query.hasNot [ Selector.style [ ( "flex-shrink", "0" ) ] ]
+        , test "prevents fixed width child from shrinking inside a row" <|
+            \() ->
+                Element.layout stylesheet
+                    (row None
+                        []
+                        [ el None [] empty
+                        , el None [ width (px 200) ] empty
+                        ]
+                    )
+                    |> Query.fromHtml
+                    |> Query.has [ Selector.style [ ( "flex-shrink", "0" ) ] ]
+        , test "allows child with unspecified width to shrink inside a row" <|
+            \() ->
+                Element.layout stylesheet
+                    (row None
+                        []
+                        [ el None [] empty
+                        , el None [] empty
+                        ]
+                    )
+                    |> Query.fromHtml
+                    |> Query.hasNot [ Selector.style [ ( "flex-shrink", "0" ) ] ]
         ]
