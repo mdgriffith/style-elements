5057b9084afbd6a23ae9d92169a3ebaeda59ead5
Matthew Griffith <mdg.griffith@gmail.com>
2017-06-15 11:30:04 -0400

    Generated CSS viewer

---
+++ tests/StyleSheetTest.elm
@@ -0,0 +1,68 @@
+module VisualTest exposing (..)
+
+import Color
+import Element exposing (..)
+import Element.Attributes exposing (..)
+import Element.Keyed
+import Html
+import Style exposing (..)
+import Style.Background as Background
+import Style.Border as Border
+import Style.Color as Color
+import Style.Font as Font
+import Style.Shadow as Shadow
+import Style.Transition as Transition
+
+
+(=>) =
+    (,)
+
+
+type Styles
+    = None
+    | TestStyle
+
+
+type Vary
+    = Other
+
+
+options =
+    [ Style.unguarded
+    ]
+
+
+stylesheet : StyleSheet Styles Vary
+stylesheet =
+    Style.stylesheetWith options
+        [ style None []
+        , style TestStyle
+            [ Border.all 1
+            , Color.text Color.darkCharcoal
+            , variation Other
+                [ Color.text Color.red
+                , hover
+                    [ Color.text Color.blue
+                    ]
+                ]
+            ]
+        ]
+
+
+main =
+    Html.program
+        { init = ( 0, Cmd.none )
+        , update = update
+        , view = view
+        , subscriptions = \_ -> Sub.none
+        }
+
+
+update msg model =
+    ( model, Cmd.none )
+
+
+view model =
+    Html.pre []
+        [ Html.text stylesheet.css
+        ]
