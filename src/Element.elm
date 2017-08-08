cdd0540c1bf7eab9a7d9fc02a6d34e64b7675207
Matthew Griffith <mdg.griffith@gmail.com>
2017-08-08 16:44:42 -0400

    keep order of whenJust

--- src/Element.elm
+++ src/Element.elm
@@ -873,7 +873,7 @@ when bool elm =
 
 {-| Another helper function that defaults to `empty`
 
-    whenJust text (Just ("Hi!"))
+    whenJust (Just ("Hi!")) text
 
 is sugar for
 
@@ -884,8 +884,8 @@ is sugar for
             text x
 
 -}
-whenJust : (a -> Element style variation msg) -> Maybe a -> Element style variation msg
-whenJust view maybe =
+whenJust : Maybe a -> (a -> Element style variation msg) -> Element style variation msg
+whenJust maybe view =
     case maybe of
         Nothing ->
             empty
