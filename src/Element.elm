08160653ea1d515c5a62eff8a1df1201151b5543
Matthew Griffith <mdg.griffith@gmail.com>
2017-09-13 07:32:51 -0400

    move Location to Element

--- src/Element.elm
+++ src/Element.elm
@@ -56,6 +56,14 @@ module Element
         , screen
           -- , numbered
           -- , bulleted
+        , nav
+        , navColumn
+        , header
+        , mainContent
+        , footer
+        , sidebar
+        , search
+        , modal
         , layout
         , viewport
         , toHtml
@@ -143,6 +151,15 @@ In CSS terms, this positions children using 'position:absolute'. So, to position
 ## Markup
 
 @docs node, button, hairline, article, section, aside, spacer, circle
+
+
+## Significant Locations
+
+@docs nav, navColumn
+
+@docs header, mainContent, footer, sidebar, search
+
+@docs modal
 
 
 ## Headings
@@ -1187,3 +1204,146 @@ An analog of `Html.map`.
 map : (a -> msg) -> Element style variation a -> Element style variation msg
 map =
     Internal.mapMsg
+
+
+{-| An area that houses the controls for running a search.
+
+While `Element.Input.search` will create a literal search input,
+this element is meant to group all the controls that are involved with searching,
+such as filters and the search button.
+
+-}
+search : style -> List (Attribute variation msg) -> Element style variation msg -> Element style variation msg
+search style attrs child =
+    Internal.Element
+        { node = "div"
+        , style = Just style
+        , attrs = Attr.width Attr.fill :: Attr.attribute "role" "search" :: attrs
+        , child = child
+        , absolutelyPositioned = Nothing
+        }
+
+
+{-| The main navigation of the site, rendered as a row.
+
+The required `name` is used by accessibility software to describe to non-sighted users what this navigation element pertains to.
+
+Don't leave `name` blank, even if you just put *"Main Navigation"* in it.
+
+-}
+nav : style -> List (Attribute variation msg) -> { options : List (Element style variation msg), name : String } -> Element style variation msg
+nav style attrs { options, name } =
+    Internal.Element
+        { node = "nav"
+        , style = Nothing
+        , attrs = [ Attr.attribute "role" "navigation", Attr.attribute "aria-label" name ]
+        , child =
+            Internal.Layout
+                { node = "ul"
+                , style = Just style
+                , layout = Style.FlexLayout Style.GoRight []
+                , attrs = attrs
+                , children =
+                    options
+                        |> List.map (Modify.setNode "li")
+                        |> Internal.Normal
+                , absolutelyPositioned = Nothing
+                }
+        , absolutelyPositioned = Nothing
+        }
+
+
+{-| -}
+navColumn : style -> List (Attribute variation msg) -> { options : List (Element style variation msg), name : String } -> Element style variation msg
+navColumn style attrs { options, name } =
+    Internal.Element
+        { node = "nav"
+        , style = Nothing
+        , attrs = [ Attr.attribute "role" "navigation", Attr.attribute "aria-label" name ]
+        , child =
+            Internal.Layout
+                { node = "ul"
+                , style = Just style
+                , layout = Style.FlexLayout Style.Down []
+                , attrs = attrs
+                , children =
+                    options
+                        |> List.map (Modify.setNode "li")
+                        |> Internal.Normal
+                , absolutelyPositioned = Nothing
+                }
+        , absolutelyPositioned = Nothing
+        }
+
+
+{-| This is the main page header area.
+-}
+header : style -> List (Attribute variation msg) -> Element style variation msg -> Element style variation msg
+header style attrs child =
+    Internal.Element
+        { node = "header"
+        , style = Just style
+        , attrs = Attr.width Attr.fill :: Attr.attribute "role" "banner" :: attrs
+        , child = child
+        , absolutelyPositioned = Nothing
+        }
+
+
+{-| This is the main page footer where your copyright and other infomation should live!
+
+<footer, role=contentinfo>
+
+-}
+footer : style -> List (Attribute variation msg) -> Element style variation msg -> Element style variation msg
+footer style attrs child =
+    Internal.Element
+        { node = "footer"
+        , style = Just style
+        , attrs = Attr.width Attr.fill :: Attr.attribute "role" "contentinfo" :: attrs
+        , child = child
+        , absolutelyPositioned = Nothing
+        }
+
+
+{-| This is a sidebar which contains complementary information to your main content.
+
+It's rendered as a column.
+
+-}
+sidebar : style -> List (Attribute variation msg) -> List (Element style variation msg) -> Element style variation msg
+sidebar style attrs children =
+    Internal.Layout
+        { node = "aside"
+        , style = Just style
+        , layout = Style.FlexLayout Style.Down []
+        , attrs = Attr.attribute "role" "complementary" :: attrs
+        , children = Internal.Normal children
+        , absolutelyPositioned = Nothing
+        }
+
+
+{-| This is the main content of your page.
+-}
+mainContent : style -> List (Attribute variation msg) -> Element style variation msg -> Element style variation msg
+mainContent style attrs child =
+    Internal.Element
+        { node = "main"
+        , style = Just style
+        , attrs = Attr.width Attr.fill :: Attr.height Attr.fill :: Attr.attribute "role" "main" :: attrs
+        , child = child
+        , absolutelyPositioned = Nothing
+        }
+
+
+{-| This is a modal
+-}
+modal : style -> List (Attribute variation msg) -> Element style variation msg -> Element style variation msg
+modal style attrs child =
+    screen <|
+        Internal.Element
+            { node = "div"
+            , style = Just style
+            , attrs = Attr.attribute "role" "alertdialog" :: Attr.attribute "aria-modal" "true" :: attrs
+            , child = child
+            , absolutelyPositioned = Nothing
+            }
