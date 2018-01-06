a62999c723fc73f6b2483d66944bbd559fb846f2
Matthew Griffith <mdg.griffith@gmail.com>
2018-01-06 09:58:26 -0500

    Element.Area docs

--- experiments/Aligned/src/Element/Area.elm
+++ experiments/Aligned/src/Element/Area.elm
@@ -1,51 +1,107 @@
-module Element.Area exposing (..)
+module Element.Area
+    exposing
+        ( announce
+        , announceUrgently
+        , aside
+        , footer
+        , heading
+        , mainContent
+        , navigation
+        )
+
+{-|
+
+
+# Area Annotations
+
+This module is meant to make accessibility easy! They're sign posts that accessibility software like screen readers can use to navigate your app.
+
+All you have to do is add them to elements in your app where you see fit.
+
+Here's an example of annotating your navigation area:
+
+    import Element.Area as Area
+
+    myNavigation =
+        Element.row [ Area.navigation ]
+            [-- ..your navigation links
+            ]
+
+@docs mainContent, navigation, heading
+
+@docs announce, announceUrgently, aside, footer
+
+-}
+
+import Element exposing (Attribute)
+import Internal.Model as Internal exposing (Description(..))
+
 
 {-| -}
-
-import Internal.Model exposing (..)
-
-
-primary : Attribute msg
-primary =
-    Describe Main
+mainContent : Attribute msg
+mainContent =
+    Internal.Describe Main
 
 
+{-| -}
 aside : Attribute msg
 aside =
-    Describe Complementary
+    Internal.Describe Complementary
 
 
+{-| -}
 navigation : Attribute msg
 navigation =
-    Describe Navigation
+    Internal.Describe Navigation
 
 
 
 -- form : Attribute msg
 -- form =
---     Describe Form
-
-
+--     Internal.Describe Form
 -- search : Attribute msg
 -- search =
---     Describe Search
+--     Internal.Describe Search
 
 
+{-| -}
 footer : Attribute msg
 footer =
-    Describe ContentInfo
+    Internal.Describe ContentInfo
 
 
+{-| This will mark an element as `h1`, `h2`, etc where possible.
+
+Though it's also smart enough to not conflict with existing nodes.
+
+So, this code
+
+    link [ Area.heading 1 ]
+        { url = "http://fruits.com"
+        , label = text "Best site ever"
+        }
+
+will generate
+
+    <a href="http://fruits.com">
+        <h1>Best site ever</h1>
+    </a>
+
+-}
 heading : Int -> Attribute msg
 heading =
-    Describe << Heading
+    Internal.Describe << Heading
 
 
+{-| Screen readers will announce changes to this element and potentially interrupt any other announcement.
+-}
 announceUrgently : Attribute msg
 announceUrgently =
-    Describe LiveAssertive
+    Internal.Describe LiveAssertive
 
 
+{-| Screen readers will announce when changes to this element are made.
+-}
 announce : Attribute msg
 announce =
-    Describe LivePolite
+    Internal.Describe LivePolite
