888d7093250bd3cc11a0acd376fb88469fbaaaad
Matthew Griffith <mdg.griffith@gmail.com>
2018-01-07 13:04:46 -0500

    Palette exploration

--- experiments/Aligned/src/Element/Palette.elm
+++ experiments/Aligned/src/Element/Palette.elm
@@ -9,40 +9,89 @@ module Element.Palette exposing (..)
 -}
 
 import Color exposing (Color)
-import Html exposing (Html)
+import Internal.Model as Internal
 
 
-{- A General example using Color.background
+{-
+
+
+   A General example using Color.background
+
+
+       type alias CustomPalette =
+           { primary : Palette.Protected Color
+           }
+
+       makeMyPalette : Palette CustomPalette
+       makeMyPalette =
+           Palette.colors CustomPalette
+               |> Palette.color purple
 
 
 
-   -- pulling a value from the palette
-   el
-       [ Color.background .primary
-       ]
-       (text "I'm styled")
+   And in View code
 
-   -- specifying a dynamic value
-   el
-       [ Color.background (dynamic blue)
-       ]
-       (text "I'm styled")
+         -- pulling a value from the palette
+         el
+             [ Color.background .primary
+             ]
+             (text "I'm styled")
+
+         -- specifying a dynamic value
+         el
+             [ Color.background (dynamic blue)
+             ]
+             (text "I'm styled")
 
 
 
-   -- If the type signature is different, this should be possile
-   -- this would be the default
-   el
-       [ Color.background blue
-       ]
-       (text "I'm styled")
+         -- If the type signature is different, this should be possile
+         -- this would be the default
+         el
+             [ Color.background blue
+             ]
+             (text "I'm styled")
 
+
+
+
+      Values to start with
+
+      * Colors -> renders as backgorund, text, and border colors
+      * FontSizes ->
+          (no reason these should be dynamic)  They can scale globally based on window size or something if needbe, using linear scaling!
+      * FontFamilies ->
+          (no reason these should be dynamic)
+      * Border Sizes
+      * Border Radius
+      * Spacing/Padding
 
 -}
 
 
-type Element color msg
+type InternalAttribute color fonts msg
+    = InternalAttribute color fonts msg
+
+
+type InternalElement color msg
     = Element msg
+
+
+
+-- type Attribute color msg
+--     = ColorStyle color
+
+
+{-| Concrete Values
+-}
+type alias Attr msg =
+    InternalAttribute Color (List Internal.Font) msg
+
+
+{-| Palette based attributes
+-}
+type alias Attribute colors fonts msg =
+    InternalAttribute (colors -> Protected Color) (fonts -> Protected Internal.Font) msg
 
 
 {-| A way of protecting values so that the user can't construct them.
@@ -52,136 +101,139 @@ type Protected thing
     | Dynamic thing
 
 
-dynamic : a -> b -> Protected a
+dynamic : Color -> Colors colors -> Protected Color
 dynamic value palette =
     Dynamic value
 
 
-
--- repaint : Palette -> Element color msg -> Element color msg
--- repaint palette el =
---     el
-
-
-type Palette colors
+type Palette colors fonts
     = Palette
-        { colors : colors
-
-        -- , colorName : colors -> Protected Color
-        , rendered : String
+        { colors : Colors colors
+        , fontSizes : Scale
+        , fontFamilies : Fonts fonts
+        , spacing : Scale
         }
 
 
-type SpacingPalette
-    = SpacingPalette (List Int)
+type Scale
+    = Linear Int
+    | Modular
+        { base : Int
+        , factor : Float
+        }
 
 
-type alias ColorPalette =
-    { primary : Protected Color }
+type Colors a
+    = Colors
+        { protected : a
+        , values : List Color
+        }
 
 
-formatColor : Color -> String
-formatColor color =
+colors : a -> Colors a
+colors a =
+    Colors
+        { protected = a
+        , values = []
+        }
+
+
+color : Color -> Colors (Protected Color -> a) -> Colors a
+color color pal =
     let
-        { red, green, blue, alpha } =
-            Color.toRgb color
+        addColor p =
+            { protected = p.protected (Protected color)
+            , values = color :: p.values
+            }
     in
-    ("rgba(" ++ toString red)
-        ++ ("," ++ toString green)
-        ++ ("," ++ toString blue)
-        ++ ("," ++ toString alpha ++ ")")
+    map addColor pal
 
 
-
--- myPalette : Palette { primary : Color }
--- myPalette =
---     Palette
---         { colors =
---             { primary = Color.blue
---             , secondary = Color.red
---             }
---         , renderColors =
---             \colors ->
---                 formatColor colors.primary
---                     ++ formatColor colors.secondary
---         }
+map : ({ protected : a, values : List Color } -> { protected : a1, values : List Color }) -> Colors a -> Colors a1
+map fn pal =
+    case pal of
+        Colors a ->
+            Colors (fn a)
 
 
-colorsPalette =
-    { primary = Color.blue
-    , secondary = Color.red
-    }
-
-
-makePalette : a -> (a -> List Color) -> Palette a
-makePalette colors get =
-    Palette
-        { colors =
-            colors
-        , rendered =
-            get colors
-                |> List.foldr (\color acc -> formatColor color ++ acc) ""
+type Fonts a
+    = Fonts
+        { protected : a
+        , values : List FontFamily
         }
 
 
-layout : Palette color -> Element color msg -> Html msg
-layout palette el =
-    Html.div []
-        [ Html.node "style" [] [ Html.text <| paletteToStyle palette ]
-        , renderEl palette el
-        ]
+type FontFamily
+    = FontFamily (List Internal.Font)
 
 
-paletteToStyle : Palette color -> String
-paletteToStyle (Palette { colors, rendered }) =
-    rendered
+fonts : a -> Fonts a
+fonts a =
+    Fonts
+        { protected = a
+        , values = []
+        }
 
 
-renderEl palette el =
-    Html.text ""
+family : FontFamily -> Fonts (Protected FontFamily -> a) -> Fonts a
+family fam pal =
+    let
+        addFontFamily p =
+            { protected = p.protected (Protected fam)
+            , values = fam :: p.values
+            }
+    in
+    mapFont addFontFamily pal
+
+
+mapFont : ({ protected : a, values : List FontFamily } -> { protected : a1, values : List FontFamily }) -> Fonts a -> Fonts a1
+mapFont fn pal =
+    case pal of
+        Fonts a ->
+            Fonts (fn a)
 
 
 
-{-
-
-
-
-
-
--}
-
-
-type Attribute color msg
-    = ColorStyle color
-
-
-{-| Concrete Values
--}
-type alias Attr msg =
-    Attribute Color msg
-
-
-{-| Palette based attributes
--}
-type alias PaletteAttribute msg =
-    Attribute (ColorPalette -> Protected Color) msg
-
-
-background : color -> Attribute color msg
-background =
-    ColorStyle
-
-
-concrete : Attr msg
-concrete =
-    background Color.blue
-
-
-fromPalette : PaletteAttribute msg
-fromPalette =
-    background .primary
-
-
-overrideWithDynamic : PaletteAttribute msg
-overrideWithDynamic =
-    background (dynamic Color.blue)
+-- type Scale a
+--     = Scale
+--         { protected : a
+--         , values : List Int
+--         }
+-- type Scale2 a
+--     = Scale2
+--         { range : a
+--         , values : List Int
+--         }
+-- scale : a -> Scale a
+-- scale a =
+--     Scale
+--         { protected = a
+--         , values = []
+--         }
+-- int : Int -> Scale (Protected Int -> a) -> Scale a
+-- int i pal =
+--     let
+--         addInt p =
+--             { protected = p.protected (Protected i)
+--             , values = i :: p.values
+--             }
+--     in
+--     mapScale addInt pal
+-- range : Int -> Int -> Scale (Protected Int -> a) -> Scale a
+-- range i j pal =
+--     let
+--         addInt val scale =
+--             mapScale
+--                 (\sc ->
+--                     { protected = sc.protected (Protected val)
+--                     , values = val :: sc.values
+--                     }
+--                 )
+--                 scale
+--     in
+--     List.foldl addInt pal (List.range i j)
+-- mapScale : ({ protected : a, values : List Int } -> { protected : a1, values : List Int }) -> Scale a -> Scale a1
+-- mapScale fn pal =
+--     case pal of
+--         Scale a ->
+--             Scale (fn a)
