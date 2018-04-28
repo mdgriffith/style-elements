ecfdb0822d66e90fef76af6563b1013598c08768
Matthew Griffith <mdg.griffith@gmail.com>
2018-04-28 12:21:41 -0400

    improve formatting on column align test

--- experiments/Aligned/tests/Tests/ColumnAlignment.elm
+++ experiments/Aligned/tests/Tests/ColumnAlignment.elm
@@ -33,15 +33,15 @@ container =
 view =
     let
         colContainer attrs children =
-            column ([ spacing 20, width (px 100), height (px 500) ] ++ attrs) children
+            column ([ Background.color lightGrey, spacing 20, width (px 100), height (px 500) ] ++ attrs) children
 
         tallContainer attrs children =
-            column ([ spacing 20, width (px 100), height fill ] ++ attrs) children
+            column ([ Background.color lightGrey, spacing 20, width (px 100), height fill ] ++ attrs) children
     in
     column
-        [ width fill ]
+        [ width fill, spacing 20 ]
         [ el [] (text "Alignment Within a Column")
-        , row []
+        , row [ spacing 20 ]
             [ colContainer []
                 [ box [] ]
             , colContainer []
@@ -55,7 +55,7 @@ view =
                 , box [ alignRight, label "third" ]
                 ]
             ]
-        , row []
+        , row [ spacing 20 ]
             [ colContainer []
                 [ box []
                 , box []
@@ -78,7 +78,7 @@ view =
                 ]
             ]
         , text "centerY"
-        , row []
+        , row [ spacing 20 ]
             [ colContainer [ height fill ]
                 [ box [ centerY, label "solo" ]
                 ]
@@ -94,7 +94,7 @@ view =
                 ]
             ]
         , text "multiple centerY"
-        , row [ height (px 800) ]
+        , row [ height (px 800), spacing 20 ]
             [ tallContainer []
                 [ box []
                 , box [ centerY ]
@@ -115,7 +115,7 @@ view =
                 ]
             ]
         , text "top, center, bottom"
-        , row []
+        , row [ spacing 20 ]
             [ colContainer []
                 [ box [ alignTop, label "first" ]
                 , box []
@@ -133,7 +133,7 @@ view =
                 ]
             ]
         , el [ width fill ] (text "Column in a Row")
-        , row [ width fill, spacing 20, label "row" ]
+        , row [ width fill, spacing 20, label "row", spacing 20 ]
             [ box [ alignLeft, alignTop, label "box" ]
             , column
                 [ alignLeft
